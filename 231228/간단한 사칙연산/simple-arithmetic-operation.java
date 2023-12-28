46c5a3a32f79bf9997a61668a7895400e2cad0ac
hyunwoosim <150868630+hyunwoosim@users.noreply.github.com>
2023-12-28 17:27:24 +0900

    Title: 간단한 사칙연산, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 231228/간단한 사칙연산/simple-arithmetic-operation.java
+++ 231228/간단한 사칙연산/simple-arithmetic-operation.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        int a =2, b=5;
+      Scanner sc = new Scanner(System.in);
+    int  a = sc.nextInt();
+    int b = sc.nextInt();      
         System.out.println(a+b);
         System.out.println(a-b);
         System.out.println(a/b);
