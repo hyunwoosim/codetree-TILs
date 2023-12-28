ab7b29612259adb1496bf4ef9269a25450098f18
hyunwoosim <150868630+hyunwoosim@users.noreply.github.com>
2023-12-28 18:45:06 +0900

    Title: 음수 구별하기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 231228/음수 구별하기/separate-negative-number.java
+++ 231228/음수 구별하기/separate-negative-number.java
@@ -5,8 +5,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a =sc.nextInt();
 
-        if(a<0){
             System.out.println(a);
+        if(a<0){
             System.out.println("minus");
         }
 
