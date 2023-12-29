cdd499294db518aa72790bf0ba60d7493927f12e
hyunwoosim <150868630+hyunwoosim@users.noreply.github.com>
2023-12-29 19:20:32 +0900

    Title: 특정 조건 두 정수 비교, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231229/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
+++ 231229/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -9,14 +9,14 @@ public class Main {
         b = sc.nextInt();
 
         if (a<b){
-            System.out.print("1");
+            System.out.print("1 ");
         }else{
             System.out.print("0");
         }
         if(a == b) {
-            System.out.pritn("0");
+            System.out.print("1 ");
         }else{
-            System.out.print("1");
+            System.out.print("0");
         }
     }
 }
