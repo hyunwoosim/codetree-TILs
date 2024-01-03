d43a673236454580360b289996d87712358faf33
hyunwoosim <150868630+hyunwoosim@users.noreply.github.com>
2024-01-03 20:07:43 +0900

    Title: 윤년인가, Time: 152ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240103/윤년인가/is-leap-year.java
+++ 240103/윤년인가/is-leap-year.java
@@ -6,10 +6,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int y = sc.nextInt();
 
-        if(y%4 ==0 || y%100 == 0){
+         if(y%4 ==0 || y%100 != 0||  y%400 == 0){
             System.out.println("true");
-
-        }else {
+        } else{
             System.out.println("false");
         }
     }
