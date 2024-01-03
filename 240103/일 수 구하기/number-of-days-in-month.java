8642c478d0a7f29b2f5e2930f4555e859cee641f
hyunwoosim <150868630+hyunwoosim@users.noreply.github.com>
2024-01-03 20:25:42 +0900

    Title: 일 수 구하기, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

--- 240103/일 수 구하기/number-of-days-in-month.java
+++ 240103/일 수 구하기/number-of-days-in-month.java
@@ -8,16 +8,20 @@ public class Main {
 
        
 
-        if (n ==2 ){
+         if(n == 2)
             System.out.println("28");
-        }else if (n =< 7 && n % 2 ==1 ){
-            System.out.pritnln("31");
-        }else {
-            System.out.println("30");
-        }if(n >= 8 && n%2 == 0){
-            System.out.pritnln("31");
-        }else {
-            System.out.pritnln("30");
+        else if(n <= 7) {
+            if(n % 2 == 1)
+                System.out.println("31");
+            else
+                System.out.println("30");
         }
+        else {
+            if(n % 2 == 0)
+                System.out.println("31");
+            else
+                System.out.println("30");
+        }
+        
     }
 }
