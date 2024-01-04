3fb0d1ed1feb1cdb067d488448c9bcf51a8d71a6
hyunwoosim <150868630+hyunwoosim@users.noreply.github.com>
2024-01-04 20:24:49 +0900

    Title: 차례로 출력, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240104/차례로 출력/print-in-order.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 
+        for(int i = 5; i<18; i++ ){
+            System.out.printf("%d ", i);
+        }
+    }
+}
