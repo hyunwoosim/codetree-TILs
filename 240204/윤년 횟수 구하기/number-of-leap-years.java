21cfdbed6df1a80e83855a36e9d8211cd896c1a4
hyunwoosim <150868630+hyunwoosim@users.noreply.github.com>
2024-02-04 20:31:13 +0900

    Title: 윤년 횟수 구하기, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240204/윤년 횟수 구하기/number-of-leap-years.java
+++ 240204/윤년 횟수 구하기/number-of-leap-years.java
@@ -8,10 +8,8 @@ public class Main {
         int n = sc.nextInt();
 
         for(int i = 1; i<=n;i++){
-            if(i%100==0 || i%400==1){
-
-            }else if(i%4==0){
-                cnt++;
+            if((i%4==0&&i%100 !=0) || (i%400==0)){
+                cnt++;    
             }
         }
         System.out.println(cnt);
