8a3886da4d0544aa62500fbec124c0351827a217
hyunwoosim <150868630+hyunwoosim@users.noreply.github.com>
2024-02-08 19:52:39 +0900

    Title: 몫 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240208/몫 출력하기/print-share.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+       
+        
+        while(true){
+             int n = sc.nextInt();
+                int a= 0;
+                 int cnt =1;
+            if((n%2==0)){
+              a= n/2;    
+             cnt++;
+                System.out.println(a);
+            }if(cnt ==3){
+                break;
+            }
+     
+     
+        }
+    }
+}
