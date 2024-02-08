c5ce2e6d2b4d57844ff2b58a382dfbb38df7eb4a
hyunwoosim <150868630+hyunwoosim@users.noreply.github.com>
2024-02-08 19:59:32 +0900

    Title: 몫 출력하기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240208/몫 출력하기/print-share.java
+++ 240208/몫 출력하기/print-share.java
@@ -5,19 +5,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
        
         
-        for(int i = 0; i<=100; i++){
-             int n = sc.nextInt();
-                int a= 0;
-               int cnt =1;
-            if(n%2==0){
-              a= n/2;    
-              cnt++;
-                System.out.println(a);
-            }if(cnt ==3){
+        int cnt = 0;
+        
+        while(true) {
+            // 변수 선언 및 입력
+            int n;
+            n = sc.nextInt();
+
+            // n이 홀수라면 아무 작업도 하지 않고, n이 짝수라면 n/2를 출력하는 작업을 3번 한 뒤 종료합니다.
+            if(n % 2 == 1)
+                continue;
+            
+            System.out.println(n / 2);
+            cnt++;
+            
+            if(cnt >= 3)
                 break;
-            }
-     
-     
         }
     }
 }
