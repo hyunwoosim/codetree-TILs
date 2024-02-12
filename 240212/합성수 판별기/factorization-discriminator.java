654791fa6bc61219ebca272aba4175a650cc727c
hyunwoosim <150868630+hyunwoosim@users.noreply.github.com>
2024-02-12 16:06:13 +0900

    Title: 합성수 판별기, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240212/합성수 판별기/factorization-discriminator.java
+++ 240212/합성수 판별기/factorization-discriminator.java
@@ -8,8 +8,8 @@ public class Main {
         int n = sc.nextInt();
         boolean bool = false;
 
-        for(int i =2; i<=n;i++ ){
-            if(n%i == 0){
+        for(int i =2; i<=n; i++ ){
+            if(n/i == 0){
                 bool = true;
             }else
                 break;
