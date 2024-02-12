c9865ad42bd448d6968dbe2683981fd20f773a1b
hyunwoosim <150868630+hyunwoosim@users.noreply.github.com>
2024-02-12 16:25:48 +0900

    Title: 소수 판별기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240212/소수 판별기/prime-number-discriminator.java
+++ 240212/소수 판별기/prime-number-discriminator.java
@@ -5,13 +5,14 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        boolean bool = false;
+        boolean bool = true;
 
         for(int i = 2 ; i<n; i++){
             if(n % i==0){
-                bool = true;
+                bool = false;
             }
         }
+        
         if(bool == true){
             System.out.println("P");
         }else{
