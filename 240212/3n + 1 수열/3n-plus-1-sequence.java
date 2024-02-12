e1c3c42fb4ca882b2e0c22bc70fe5eb90cd251e0
hyunwoosim <150868630+hyunwoosim@users.noreply.github.com>
2024-02-12 15:23:56 +0900

    Title: 3n + 1 수열, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240212/3n + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt = 0;
+        while(true){
+           if(n==1){
+            break;
+           }
+
+            if(n%2==0){
+                n=n/2;
+                cnt++;
+            }
+            else {
+                n=n*3+1;
+                cnt++;
+            } 
+            }   
+            System.out.println(cnt);
+        }
+        
+    }
