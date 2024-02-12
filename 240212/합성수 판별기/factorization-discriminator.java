2aed045713a2de70f6e9a5afc942096c8cb8e756
hyunwoosim <150868630+hyunwoosim@users.noreply.github.com>
2024-02-12 16:07:07 +0900

    Title: 합성수 판별기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240212/합성수 판별기/factorization-discriminator.java
+++ 240212/합성수 판별기/factorization-discriminator.java
@@ -8,11 +8,10 @@ public class Main {
         int n = sc.nextInt();
         boolean bool = false;
 
-        for(int i =2; i<=n; i++ ){
-            if(n/i == 0){
+        for(int i =2; i<n; i++ ){
+            if(n%i == 0){
                 bool = true;
-            }else
-                break;
+            
         }
         if (bool == true){
             System.out.println("C");
