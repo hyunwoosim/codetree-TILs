2cf6b8cb5b91f16f50513f16fd5dc25e04968452
hyunwoosim <150868630+hyunwoosim@users.noreply.github.com>
2024-02-20 20:09:21 +0900

    Title: 별표 출력하기 5, Time: 180ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240220/별표 출력하기 5/print-star-5.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+      
+        int n = sc.nextInt();
+
+        
+        for(int i = n; i >= 1; i--) {
+          
+            for(int j = 1; j <= i; j++) {
+            
+                for(int k = 1; k <= i; k++)
+                    System.out.print("*");
+                
+                
+                System.out.print(" ");
+            }
+        
+            System.out.println();
+        }
+    }
+}
