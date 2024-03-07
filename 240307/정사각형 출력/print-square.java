07c27f59e64a6617b3fdaf2ad75d88c22a7413e2
hyunwoosim <150868630+hyunwoosim@users.noreply.github.com>
2024-03-07 20:20:35 +0900

    Title: 정사각형 출력, Time: 245ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240307/정사각형 출력/print-square.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt =1;
+
+        for(int i =0; i < n; i++){
+            for(int q = 0; q < n; q++){
+                System.out.print(cnt + " ");
+                cnt++;
+            }
+            System.out.println();
+        }
+    }
+}
