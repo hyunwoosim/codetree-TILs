cd343049b5f2fcf64fec8e36fa3032ac16c27956
hyunwoo <150868630+hyunwoosim@users.noreply.github.com>
2024-03-09 21:31:46 +0900

    Title: 구구단 만들기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240309/구구단 만들기/print-multiplication-table.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+
+        for(int i = 1; i <=9; i++){
+            for(int q = 10; q >=a; q--){
+                if(q<=b && q%2==0){
+                System.out.print(q + " * " + i + " = " + (i*q));
+                
+                if( a<q ){
+                    System.out.print(" / ");
+                }
+                }
+                }
+           System.out.println();
+            }
+           
+        }
+
+       
+
+    }
