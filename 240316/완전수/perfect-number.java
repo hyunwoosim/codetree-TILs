a89f8182553e19d69acd68d4954c66afff97f3b4
hyunwoo <150868630+hyunwoosim@users.noreply.github.com>
2024-03-16 18:32:11 +0900

    Title: 완전수, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240316/완전수/perfect-number.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int s = sc.nextInt();
+        int e = sc.nextInt();
+        int cnt = 0;
+        int sum =0;
+
+        for(int i = s; i <=e; i++){
+            for( int q = 1; q <= i-1; q++){
+                if(i %q == 0){
+                    sum +=q;
+                }
+            }
+                if(sum == i){
+                    cnt++;
+                }
+
+        }
+        System.out.print(cnt);
+    }
+}
