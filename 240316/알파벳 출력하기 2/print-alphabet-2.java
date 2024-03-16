d83e8178ca57bb0a194e1606fe00240febd2297b
hyunwoo <150868630+hyunwoosim@users.noreply.github.com>
2024-03-16 17:51:22 +0900

    Title: 알파벳 출력하기 2, Time: 117ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240316/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int cnt = 'A';
+
+        for(int i = n; i>= 1; i--){
+            for(int q = n; q>= 1; q-- ){
+                if(q>i){
+                    System.out.print("  ");
+                }else{
+
+                System.out.print((char)cnt+ " ");
+                cnt++;
+                }
+                if(cnt == 'Z'){
+                    System.out.print((char)cnt+ " ");
+                    cnt = 'A';
+                }
+            }
+            System.out.println();
+        }
+    }
+}
