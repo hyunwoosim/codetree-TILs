f36dc593a340296da6fc9c0d301f2e875a37a84f
hyunwoo <150868630+hyunwoosim@users.noreply.github.com>
2024-03-16 18:46:44 +0900

    Title: 소수 구하기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240316/소수 구하기/get-prime.java
+++ 240316/소수 구하기/get-prime.java
@@ -3,13 +3,19 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
+    // 변수 선언 및 입력
+        int n = sc.nextInt();
 
-        int n =sc.nextInt();
-
-        for(int i =2; i <=n; i++){
-            if(n/i != 0){
-                System.out.print(i+ " ");
-            }
+        // 1부터 n까지 소수를 구합니다.
+        for(int i = 1; i <= n; i++) {
+            if(i == 1) continue;
+            boolean isprime = true;
+            
+            for(int j = 2; j < i; j++)
+                if(i % j == 0) isprime = false;
+            
+            if(isprime)
+                System.out.print(i + " ");
         }
     }
 }
