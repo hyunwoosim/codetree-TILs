a61cfe6ae61904be1e55f7c8ac08938bde165984
hyunwoo <150868630+hyunwoosim@users.noreply.github.com>
2024-03-25 20:28:47 +0900

    Title: 연속부분수열일까, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240325/연속부분수열일까/contiguous-array-or-not.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n1= sc.nextInt();
+        int n2= sc.nextInt();
+        int[] A = new int[100];
+        int[] B = new int[100];
+        int cnt = 0;
+        for(int i = 0; i < n1; i++){
+            A[i] = sc.nextInt();
+        }
+        for(int i = 0; i < n2; i++){
+            B[i] = sc.nextInt();
+        }
+        for(int i = 0; i< n1; i++){
+
+            for(int q = 0; q< n2; q++){
+                if(A[i] == B[q]&& A[i+1]==B[q+1]){
+                   cnt++;
+                }
+               
+            }
+        }
+        if(cnt ==2){
+            System.out.print("Yes");
+        }else{
+            System.out.print("No");
+        }
+    }
+}
