a8747d33e0138d8c1868102d0c92c630d23c91de
hyunwoo <150868630+hyunwoosim@users.noreply.github.com>
2024-03-25 20:30:54 +0900

    Title: 연속부분수열일까, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240325/연속부분수열일까/contiguous-array-or-not.java
+++ 240325/연속부분수열일까/contiguous-array-or-not.java
@@ -24,7 +24,7 @@ public class Main {
                
             }
         }
-        if(cnt ==2){
+        if(cnt >=2){
             System.out.print("Yes");
         }else{
             System.out.print("No");
