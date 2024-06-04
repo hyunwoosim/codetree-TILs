8ce98c57d25adbfbbeadc2609cacf74e2ec5733d
hyunwoo <150868630+hyunwoosim@users.noreply.github.com>
2024-06-04 18:21:54 +0900

    Title: 공백을 기준으로 출력, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240604/공백을 기준으로 출력/output-based-on-space.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        char[] arr = new char[100];
+        int count =0;
+
+        for(int i = 0; i < str1.length(); i++){
+           if(str1.charAt(i) != ' '){
+           arr[count] = str1.charAt(i);
+            count++;
+           
+           }
+        }     
+
+        for(int i = 0; i < str2.length(); i++){
+           if(str2.charAt(i) != ' '){
+           arr[count] = str2.charAt(i);
+            count++;
+           
+           }
+        }   
+        for(int i = 0; i < count; i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
