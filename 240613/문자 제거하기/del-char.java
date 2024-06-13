eb216aa1d12ab59739dbbe53a147f40f4c06c0b6
hyunwoo <150868630+hyunwoosim@users.noreply.github.com>
2024-06-13 16:13:21 +0900

    Title: 문자 제거하기, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240613/문자 제거하기/del-char.java
+++ 240613/문자 제거하기/del-char.java
@@ -3,24 +3,28 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
+         // 문자열을 입력받습니다.
         String str = sc.next();
-        int len = str.length();
-        
-        for(int i = 0; i < 20; i++){
-            int num = sc.nextInt();
-         
 
-            if(num <= len){
-                str = str.substring(0,num) + str.substring(num+1);
-                System.out.println(str);
-                len--;
-            }
-            if(num > len) {
-                str = str.substring(0,len-1) ;
-                break;
-            }
+        // 문자열의 길이를 구합니다.
+        int len = str.length();
+
+        // 문자가 하나 남을 때까지 반복합니다.
+        while(len > 1) {
+            // 정수를 입력받습니다.
+            int a = sc.nextInt();
+            
+            // 정수가 문자열의 길이 이상이면 마지막 문자를 가리키게 변경합니다.
+            if(a >= len)
+                a = len - 1;
+            
+            // a번 자리의 원소를 제거합니다.
+            str = str.substring(0, a) + str.substring(a + 1);
+            len--;
+            
+            // 현재 문자열을 출력합니다.
+            System.out.println(str);
         }
-        System.out.println(str);
 
     }
 }
